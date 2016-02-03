f47911db40161f06fc9098b0964e441094c25985
GLNRO <lg@glnro.com>
2016-02-03 14:10:19 -0500

    Create three persistent models

--- Meteor/test-app/test-app.js
+++ Meteor/test-app/test-app.js
@@ -1,4 +1,6 @@
 Colors = new Mongo.Collection("colors");
+Temps = new Mongo.Collection("temps");
+Shapes = new Mongo.Collection("shapes");
 
 
 if (Meteor.isClient) {
@@ -6,6 +8,18 @@ if (Meteor.isClient) {
   Template.body.helpers({
     colors: function(){
       return Colors.find({});
+    }
+  });
+
+  Template.body.helpers({
+    temps: function(){
+      return Temps.find({});
+    }
+  });
+
+  Template.body.helpers({
+    shapes: function(){
+      return Shapes.find({});
     }
   });
 
@@ -22,23 +36,54 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.body.helpers({
-    states: [
-      { weather: "sunny "},
-      { weather: "cloudy"},
-      { weather: "mystery"}
-    ]
+  Template.body.events({
+    "submit .new-temp": function (event){
+      event.preventDefault();
+      var description = event.target.description.value;
+
+      Temps.insert({
+        description: description,
+        createdAt: new Date()
+      })
+      event.target.description.value="";
+    }
   });
 
-  Template.body.helpers({
-    shapes: [
-      {glyph: "circle"},
-      {glyph: "octagon"},
-      {glyph: "rectangle"},
-      {glyph: "triangle"}
-    ]
+  Template.body.events({
+    "submit .new-shape": function (event){
+      event.preventDefault();
+      var glyph = event.target.glyph.value;
+
+      Shapes.insert({
+        glyph: glyph,
+        createdAt: new Date()
+      })
+      event.target.glyph.value="";
+    }
+  })
+
+  Template.color.events({
+    "click .toggle-checked": function (event){
+      Colors.update(this._id,{
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function (event){
+      Colors.remove(this._id);
+    }
   });
 
+  Template.temp.events({
+    "click .delete": function (event){
+      Temps.remove(this._id);
+    }
+  })
+
+  Template.shape.events({
+    "click .delete": function (event){
+      Shapes.remove(this._id);
+    }
+  })
 
 
 
